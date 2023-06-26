5d60537620f1f8c764b0db1d87486873414bfcc0
zahid <zahidhasmani07@gmail.com>
2023-06-26 21:06:12 +0000

    !fixup!

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -129,7 +129,7 @@ BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 BOARD_RAMDISK_USE_LZ4 := true
 TARGET_KERNEL_ADDITIONAL_FLAGS := BRAND_SHOW_FLAG=oneplus
 TARGET_KERNEL_SOURCE := kernel/oneplus/sm8250
-TARGET_KERNEL_CONFIG := evo_defconfig vendor/debugfs.confi
+TARGET_KERNEL_CONFIG := vendor/kona-perf_defconfig
 
 # Lineage Health
 TARGET_HEALTH_CHARGING_CONTROL_CHARGING_PATH := /sys/class/oplus_chg/battery/mmi_charging_enable
