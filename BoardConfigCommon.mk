8e579869d61ba966392d74bdfdcf7319aeaf1c7c
zahid <zahidhasmani07@gmail.com>
2023-07-01 22:03:07 +0000

    add to fix oos 13.1 issue

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -60,6 +60,7 @@ TARGET_BOOTLOADER_BOARD_NAME := kona
 
 # Camera
 TARGET_USES_OPLUS_CAMERA := true
+TARGET_ALTERNATIVE_FUTEX_WAITERS := true
 
 # Properties
 TARGET_ODM_PROP += $(COMMON_PATH)/odm.prop
@@ -130,7 +131,7 @@ BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 BOARD_RAMDISK_USE_LZ4 := true
 TARGET_KERNEL_ADDITIONAL_FLAGS := BRAND_SHOW_FLAG=oneplus
 TARGET_KERNEL_SOURCE := kernel/oneplus/sm8250
-TARGET_KERNEL_CONFIG := vendor/kona-perf_defconfig
+TARGET_KERNEL_CONFIG := vendor/kona-perf_defconfig vendor/debugfs.config
 
 # Clang
 TARGET_KERNEL_CLANG_COMPILE := true
