21a527b4eaa54ef3ebccf887d562fab629f30d1b
RealJohnGalt <RealJohnGalt@users.noreply.github.com>
2023-07-03 18:25:05 +0530

    opkona: Add support for Automatic HBM.
    This is the way AOSP handles HBM in sunlight.
    Needs a corresponding kernel patch.
    
    Change-Id: I59c5ad75134944792ac4bb37fa8400fdecb91f3e

--- common.mk
+++ common.mk
@@ -157,6 +157,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/display/display_id_4630946950956839809.xml:$(TARGET_COPY_OUT_VENDOR)/etc/displayconfig/display_id_4630946950956839809.xml
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.3.vendor \
